45d44766935eb18432a94e136086d2923d0ffc69
David Joy <Davidjoy.win@gmail.com>
2023-01-10 01:05:50 +0100

    task corrected:

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -5,12 +5,12 @@ Modele: 1-my_list
 
 
 class MyList(list):
-	"""
-	Class inherit from list
-	"""
+    """
+    Class inherit from list
+    """
 
-	def print_sorted(self):
-		"""
-		prints the list, but sorted (ascending sort)
-		"""
-		print(sorted(self))
+    def print_sorted(self):
+        """
+        prints the list, but sorted (ascending sort)
+        """
+        print(sorted(self))
